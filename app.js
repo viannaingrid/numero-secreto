dd1311266c650fa055f51711ae85b33214fd0042
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-21 18:03:52 -0300

    :wrench: config js para apareecer o texto na tela

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo advinhe o número';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+function exibirTexto(tag, texto) {
+
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto
+
+}
+
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', ' Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    console.log('click');
+}
